6f4199e81483d063220feb90f89c25dd552fbe65
DevJoshi-IITB <devrjoshi15@gmail.com>
2024-07-07 17:36:49 +0530

    week1

---
+++ Week 1/index.js
@@ -0,0 +1,49 @@
+const toTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+window.onscroll = () => {
+    const button = document.getElementById('myBtn')
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+const slides = document.querySelectorAll(".sliding-img");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider() {
+    if (slides.length > 0) {
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+}
+
+function showSlide(index) {
+    if (index >= slides.length) {
+        slideIndex = 0;
+    }
+    else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide() {
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide() {
+    slideIndex++;
+    showSlide(slideIndex);
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Web Dev
+
+## Week 1
+
+A website on chess make with basic html, css and js.
+
+## Some key elements
+
+1. Responsive Slider
+2. Playeble integrated chess game
+3. Fully functional navbar
+4. Back to Top button
